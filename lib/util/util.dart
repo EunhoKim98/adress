01ef1a37fd54d4ed618dbb3b6e1f9d86f87523ba
Glenn <100998394+EunhoKim98@users.noreply.github.com>
2024-12-17 16:28:06 +0900

    util.dart 업데이트m

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,17 +1,18 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 String KEY = 'U01TX0FVVEgyMDI0MTEyMjE0NTAyNjExNTI1NzU=';
 
 // 주소 검색
-void search(String address) async {
-  print(address);
+Future<List> search(String address) async {
   final String url = 'https://business.juso.go.kr/addrlink/addrEngApiJsonp.do';
 
   // 파라미터 설정
   final Map<String, String> params = {
     'currentPage': '1',
-    'countPerPage': '2',
+    'countPerPage': '100',
     'resultType': 'json',
     'confmKey': KEY,
     'keyword': address // 'keyword'에 'address'를 사용
@@ -25,55 +26,115 @@ void search(String address) async {
 
     if (response.statusCode == 200) {
       String jsonpResponse = response.body;
-      String jsonResponse = jsonpResponse.substring(jsonpResponse.indexOf('(') + 1, jsonpResponse.lastIndexOf(')'));
+      String jsonResponse = jsonpResponse.substring(
+          jsonpResponse.indexOf('(') + 1, jsonpResponse.lastIndexOf(')'));
 
       Map<String, dynamic> data = json.decode(jsonResponse);
       data = data['results'];
 
-      final int totalCount = int.parse(data['common']['totalCount']);
-      final int countPerPage =  int.parse(data['common']['countPerPage']);
-      final int currentPage =  int.parse(data['common']['currentPage']);
-
-      // 페이지네이션 생성
-      List pageBar = makePagination(totalCount, countPerPage, currentPage);
-      print(pageBar);
-
       // 파싱된 데이터 출력
-      print('juso');
-      print(data['juso']);
+      // print('juso');
+      return data['juso']; // 실제 데이터 반환
+
     } else {
       print('요청 실패: ${response.statusCode}');
+      return []; // 실패 시 빈 리스트 반환
     }
   } catch (e) {
     print('예외 처리: $e');
+    return []; // 예외 발생 시 빈 리스트 반환
   }
 }
 
-List makePagination(int totalCount, int countPerPage, int currentPage) {
-  final int totalPages = (totalCount / countPerPage).ceil(); // 전체 페이지 수 계산
-  List<int> pages = [];
-  int startPage, endPage;
 
-  if (totalPages <= 5) {
-    startPage = 1;
-    endPage = totalPages;
-  } else {
-    startPage = (currentPage - 2 > 0) ? currentPage - 2 : 1;
-    endPage = (currentPage + 2 < totalPages) ? currentPage + 2 : totalPages;
+class BannerAdWidget extends StatefulWidget {
+  final double width;
+  final double height;
+  final String adUnitId;
 
-    if (endPage - startPage < 4) {
-      if (startPage == 1) {
-        endPage = (totalPages < 5) ? totalPages : 5; // 시작 페이지가 1일 경우 최대 5개
-      } else if (endPage == totalPages) {
-        startPage = totalPages - 4 > 0 ? totalPages - 4 : 1; // 마지막 페이지일 경우
-      }
+  const BannerAdWidget({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.adUnitId,
+  });
+
+  @override
+  State<BannerAdWidget> createState() => _BannerAdWidgetState(
+    width: width,
+    height: height,
+    adUnitId: adUnitId,
+  );
+}
+
+class _BannerAdWidgetState extends State<BannerAdWidget> with WidgetsBindingObserver {
+  final double width;
+  final double height;
+  final String adUnitId;
+
+  BannerAd? _bannerAd;
+
+  _BannerAdWidgetState({
+    required this.width,
+    required this.height,
+    required this.adUnitId,
+  });
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _loadAd();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _loadAd();
     }
   }
 
-  for (int i = startPage; i <= endPage; i++) {
-    pages.add(i);
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox( // SizedBox 같은거로 영역을 제한하지 않으면 오류 발생할 수 있음
+      width: width,
+      height: height,
+      child: _bannerAd != null ? AdWidget(ad: _bannerAd!) : const Text(''),
+    );
   }
 
-  // 페이지네이션 HTML 생성 (여기서는 리스트로 출력)
-  return pages;
+  void _loadAd() {
+    setState(() {
+      _bannerAd = null;
+    });
+
+    BannerAd(
+      adUnitId: adUnitId,
+      request: const AdRequest(),
+      size: AdSize(
+        width: width.toInt(),
+        height: height.toInt(),
+      ),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          ad.dispose();
+        },
+        onAdOpened: (Ad ad) {},
+        onAdClosed: (Ad ad) {},
+        onAdImpression: (Ad ad) {},
+      ),
+    ).load();
+  }
 }
